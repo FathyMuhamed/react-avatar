88566e405ac1926adcb0bcafd0ae046f20f0698f
FathyMuhamed <fathimuhamed10522@gmail.com>
2022-04-11 02:46:59 +0200

    Updates

---
+++ precache-manifest.fdfbd4d00eb026ddc434f2123ee8cafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e7a5dbb09dfd05381b4371874f4c9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95be5183114ea9c93b98",
+    "url": "./static/css/main.824b163c.chunk.css"
+  },
+  {
+    "revision": "a3d33e8a5362ec7fc9f2",
+    "url": "./static/js/2.d416007a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d416007a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95be5183114ea9c93b98",
+    "url": "./static/js/main.f3f87153.chunk.js"
+  },
+  {
+    "revision": "35b933adb22383445022",
+    "url": "./static/js/runtime-main.efafe317.js"
+  }
+]);
